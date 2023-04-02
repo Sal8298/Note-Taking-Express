ffde4c79ada577532152f09a7c68d68f2b16a3c8
Sal8298 <salahuddinasim95@gmail.com>
2023-04-02 18:04:37 -0500

    Fixed Post Note function.

--- public/assets/js/noteUtility.js
+++ public/assets/js/noteUtility.js
@@ -1,17 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function createNote(body, allNotes) {
-  const notes = body;
-  allNotes.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, '../db/allNotes.json'),
-    JSON.stringify({ allNotes }, null, 2)
-  );
-  return notes;
-}
-
-module.exports =  function deleteNote(id, notes) {
+module.exports = function deleteNote(id, notes) {
   let allNotes = notes.filter(el => {
     if (el.id == id) {
       return false
@@ -23,13 +13,13 @@ module.exports =  function deleteNote(id, notes) {
   })
 
   let index = 0;
-  allNotes.forEach(notes => {
-    notes.id = index;
+  allNotes.forEach(note => {
+    note.id = index;
     index += 1;
   });
 
   fs.writeFileSync(
-    path.join(__dirname, '../db/allNotes.json'),
+    path.join(__dirname, '../../../db/allNotes.json'),
     JSON.stringify({ allNotes }, null, 2)
   );
   return allNotes;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,41 @@
 const router = require('express').Router();
-let { allNotes } = require('../db/allNotes.json');
+const fs = require('fs');
+const path = require('path');
+const deleteNote = require('../public/assets/js/noteUtility');
+let { allNotes }  = require('../db/allNotes');
  
 router.get('/notes', (req, res) => {
-  
+    let results = allNotes;
+    res.json(results);
   });
   
   router.post('/notes', (req, res) => {
-   
+    if(allNotes) {
+    req.body.id = allNotes.length.toString();
+    // console.log(req.body)
+    } else 
+    { req.body.id = 0 }
+    createNote(req.body, allNotes)
+    res.json(allNotes);
   });
   
   router.delete('/notes/:id', async (req, res) => {
-   
+    const { id } = req.params
+    allNotes = await deleteNote(id, allNotes);
+    res.json(allNotes);
   });
   
+
+  function createNote(body, allNotes) {
+    const note = body;
+    console.log('body')
+    allNotes.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, '.././db/allNotes.json'),
+      JSON.stringify({ allNotes },null, 2)
+    );
+    return note;
+  }
+
   module.exports = router;
   
