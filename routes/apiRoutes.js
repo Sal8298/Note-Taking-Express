9ea3b3452d3e07541afe5c24541c35c050e90180
Sal8298 <salahuddinasim95@gmail.com>
2023-04-02 14:24:25 -0500

    Fixed html routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
-let { allNotes } = require('../db/allNotes.json')
+let { allNotes } = require('../db/allNotes.json');
  
 router.get('/notes', (req, res) => {
-
+  
   });
   
   router.post('/notes', (req, res) => {
    
   });
   
-  // Route parameters :👌 
   router.delete('/notes/:id', async (req, res) => {
    
   });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const path = require('path');
 
 // Get Notes from page.
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // Homepage route.
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
